634203f2065145b5a46ad5f9e50d33abc1dfa0ac
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-03-01 20:56:53 +0200

    first commit - banner creation - api set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import guestBookApi from './api/guest-book';
+import Banner from './components/Banner/Banner';
 
 function App() {
+  useEffect(() => {
+    const register = async () => {
+      try {
+        const { userId, errors } = await guestBookApi.register(
+          'zzogrdiano',
+          'ahmedalirezk50@gmail.com',
+          '12345678Ae@',
+        );
+
+        console.log(userId, errors);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    // s
+    const login = async () => {
+      try {
+        const { userData, errors } = await guestBookApi.login(
+          'zzogrdianoss',
+          'ahme0@gmail.com',
+          '12345678Ae@',
+        );
+
+        console.log(userData, errors);
+      } catch (error) {
+        console.log('error -------------- ', error);
+      }
+    };
+    // s
+    const fetchMessages = async () => {
+      try {
+        const { messages, errors } = await guestBookApi.fetchMessages(
+          'zzogrdianoss',
+          'ahme0@gmail.com',
+          '12345678Ae@',
+        );
+
+        console.log(messages, errors);
+      } catch (error) {
+        console.log('error -------------- ', error);
+      }
+    };
+    // fetchMessages();
+    // s
+    const fetchMessage = async (id) => {
+      try {
+        const { message, errors } = await guestBookApi.fetchMessage(id);
+
+        console.log(message, errors);
+      } catch (error) {
+        console.log('error -------------- ', error);
+      }
+    };
+    // s
+    // s
+
+    const createMessage = async (username, content) => {
+      try {
+        const { newMessageId, errors } = await guestBookApi.createMessage(
+          username,
+          content,
+        );
+
+        console.log(newMessageId, errors);
+      } catch (error) {
+        console.log('error -------------- ', error);
+      }
+    };
+
+    // s
+    // s
+    const updateMessage = async (messageId, username, content) => {
+      try {
+        const { updatedMessageId, errors } = await guestBookApi.updateMessage(
+          messageId,
+          username,
+          content,
+        );
+
+        console.log(updatedMessageId, errors);
+      } catch (error) {
+        console.log('error -------------- ', error);
+      }
+    };
+    // updateMessage(
+    //   '621e04292a6360fc08afad7a',
+    //   'chandler',
+    //   'updated from chrome2',
+    // );
+    // s
+    // s
+
+    const deleteMessage = async (messageId, username) => {
+      try {
+        const { deletedMessageId, errors } = await guestBookApi.deleteMessage(
+          messageId,
+          username,
+        );
+
+        console.log(deletedMessageId, errors);
+      } catch (error) {
+        console.log('error -------------- ', error);
+      }
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main'>
+      <Banner />
+    </main>
   );
 }
 
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import styles from './Banner.module.css';
+import proposalImg from './proposal.jpg';
+
+const Banner = () => {
+  console.log(proposalImg);
+  return (
+    <header className={styles.header}>
+      <div className={styles.imgWrapper}>
+        <img src={proposalImg} alt='proposal' className={styles.img} />
+      </div>
+      <h1 className={styles.title}>
+        <span>Chandler & Monica</span>
+        <span className={styles.subtitle}>wedding</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Banner;
